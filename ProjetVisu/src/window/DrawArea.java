6236940ff16489db4b2181a952523d098f54d666
Rexxar <Rexxar@Jean>
2014-11-22 04:18:28 +0100

    Presque rien, je change juste de poste.

---
+++ ProjetVisu/src/window/DrawArea.java
@@ -0,0 +1,9 @@
+package window;
+
+import javax.swing.JPanel;
+
+public class DrawArea extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	
+}
--- ProjetVisu/src/window/Window.java
+++ ProjetVisu/src/window/Window.java
@@ -4,11 +4,18 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Window {
+public class Window implements Runnable {
 	private JFrame window;
+	private DrawArea view;
 	
 	public Window() {
 		window = new JFrame("Projet - Visualisation");
+		view = new DrawArea();
+	}
+
+	public void run() {
 		window.setPreferredSize(new Dimension(800, 600));
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
