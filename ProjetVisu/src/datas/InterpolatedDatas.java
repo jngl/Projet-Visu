52e5b6c92f6586f7062c39efd7fbae04fa13b60b
Rexxar <Rexxar@130.190.30.104>
2014-11-24 16:56:34 +0100

    Last version (hardy do not work)

--- ProjetVisu/src/datas/InterpolatedDatas.java
+++ ProjetVisu/src/datas/InterpolatedDatas.java
@@ -5,6 +5,13 @@ import java.util.Date;
 
 import window.Window;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.DecompositionSolver;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
 public class InterpolatedDatas {
 	public static final int ShepardMethod = 1;
 	public static final int HardyMethod = 2;
@@ -22,6 +29,9 @@ public class InterpolatedDatas {
 	private double pixelHeight;
 	private double pixelWidth;
 	
+	private double[] hardyAlpha;
+	private double hardyR=0.815;
+	
 	public InterpolatedDatas(GazDatas gazDatas, Date date, int method, double ShepardPower, int width, int height, Window window) {
 		datas = new double[height][width];
 		this.date = date;
@@ -34,6 +44,37 @@ public class InterpolatedDatas {
 		pixelWidth = (maxLongitude - minLongitude) / (double)(width - 1);
 		minValue = Double.MAX_VALUE;
 		maxValue = Double.MIN_VALUE;
+		
+		if(method == HardyMethod){
+			gazDatas.setUniqueDate(this.date);
+			
+			int N=gazDatas.getUniqueDateDatas().size();
+			double[][] matrix = new double[N][N];
+			
+			for(int line = 0; line <  N; ++line){
+				for(int column = 0; column < N; ++column){
+					
+					Point2D.Double p1 = gazDatas.getUniqueDateDatas().get(line).x;
+					Point2D.Double p2 = gazDatas.getUniqueDateDatas().get(column).x;
+					
+					matrix[line][column]=Math.sqrt(hardyR + Math.pow(p1.distance(p2),2));
+
+				}
+			}
+			
+			double[] vecF = new double[N];
+			
+			for(int i = 0; i<N; ++i){
+				vecF[i]=gazDatas.getUniqueDateDatas().get(i).y;
+			}
+			
+			RealMatrix coefficients = new Array2DRowRealMatrix(matrix, false);
+			DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
+			RealVector constants = new ArrayRealVector(vecF, false);
+			RealVector solution = solver.solve(constants);
+			
+			hardyAlpha = solution.toArray();
+		}
 		
 		for(int line = 0; line < height; ++line) {
 			for(int column = 0; column < width; ++column) {
@@ -73,7 +114,13 @@ public class InterpolatedDatas {
 	}
 	
 	private double fillDatasHardyMethod(int line, int column, GazDatas gazDatas) {
-		return 0;
+		double result=0;
+		Point2D.Double point = getEarthPostions(line, column);
+		for(int i = 0; i < gazDatas.getUniqueDateDatas().size(); ++i){
+			double hkx=Math.sqrt(hardyR+Math.pow(point.distance(gazDatas.getUniqueDateDatas().get(i).x), 2));
+			result+=hardyAlpha[i]*hkx;
+		}
+		return result;
 	}
 	
 	private Point2D.Double getEarthPostions(int line, int column) {
